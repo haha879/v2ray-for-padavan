9551fb9677f708444c39e06643850e8e438b32ae
MITUSHUSHENG <54094086+haha879@users.noreply.github.com>
2024-07-14 09:25:29 +0800

    Update hebing.py

--- py/hebing.py
+++ py/hebing.py
@@ -7,6 +7,15 @@ filedir = 'output'
 filenames=os.listdir(filedir)
 #打开当前目录下的result.txt文件，如果没有则创建
 f=open('iptv.txt','w')
+
+ # 获取当前时间
+        local_tz = pytz.timezone("Asia/Shanghai")
+        now = datetime.now(local_tz)
+        # now = datetime.now()
+        output.write(f"\n更新时间,#genre#\n")
+        output.write(f"{now.strftime("%Y-%m-%d")},url\n")
+        output.write(f"{now.strftime("%H:%M:%S")},url\n")
+
 #先遍历文件名
 for filename in filenames:
     filepath = filedir+'/'+filename
